86cd4df55470b65c14d23004af23bdde100500b4
lightnoway <lightnoway@qq.com>
2020-10-25 01:03:57 +0800

    响应.vue

--- packages/vite-diy/index.js
+++ packages/vite-diy/index.js
@@ -2,6 +2,7 @@
 const Koa = require('koa');
 const send = require('koa-send');
 const path = require('path');
+const parseSFC = require('@vue/compiler-sfc');
 
 const app = new Koa();
 
@@ -16,8 +17,6 @@ app.use(async (ctx, next) => {
         const moduleFilePathP = path.parse(moduleFilePath);
         ctx.overrideSend = [ctx, moduleFilePathP.base, { root: moduleFilePathP.dir }];
     }
-    console.log(000, ctx.path);
-    //index 非空，会路由到index指向的文件，文件不存在则报错
     return next();
 })
 
@@ -28,14 +27,41 @@ app.use(async (ctx, next) => {
     } else {
         await send(ctx, ctx.path, { root: process.cwd(), index: 'index.html' });
     }
-    //index 非空，会路由到index指向的文件，文件不存在则报错
+    //send 参数： index 非空，会路由到index指向的文件，文件不存在则报错
     return next();
 })
+//4 响应 .vue
+app.use(async (ctx, next) => {
+    if (ctx.path.endsWith('.vue')) {
+        // console.log('.vue', ctx.path);
+        //.vue 直接被响应了, 但实际需要的parse后的文件
+        // if(ctx.query.)
+        let code = await streamToString(ctx.body);
+        const parseRet = parseSFC.parse(code);
+        // console.log('parseRet', parseRet);
+        if (ctx.query.type === 'template') {
+            //template
+            code = parseSFC.compileTemplate(parseRet.descriptor).code;
+            // console.log('template========', code);
+        } else {
+            //js 
+            code = parseRet.descriptor.script.content
+                .replace('export default ', 'const __script = ') + `
+import { render as __render } from "${ctx.path}?type=template"
+__script.render = __render
+export default __script`
+        }
+        ctx.body = code;
+        ctx.type = 'application/javascript';
+    }
+    return next();
+});
+
 //2 替换import 中 字母/@ 开头的库的路径
 app.use(async (ctx, next) => {
     if (ctx.type === 'application/javascript') {
         const content = await streamToString(ctx.body); //?? 不改写body时 ERR_CONTENT_LENGTH_MISMATC
-        ctx.body = content.replace(/(import.*from\s+')([\w@])/g, `$1${modulePrefix}$2`);
+        ctx.body = content.replace(/(\s+from\s+['"])([\w@])/g, `$1${modulePrefix}$2`);
         // console.log(225, ctx.type);
     }
     return next();
@@ -51,6 +77,10 @@ app.listen(port, () => {
  * @param {*} stream 
  */
 function streamToString(stream) {
+    console.log('type stream', typeof (stream));
+    if (typeof (stream) === 'string') {
+        return stream;
+    }
     /**
      * @type ReadStream[]
      */
@@ -62,9 +92,16 @@ function streamToString(stream) {
                 resolve('');
                 return;
             }
-            /**@type ReadStream */
-            const all = chunks.reduce((a, b) => a.concat(b));
-            resolve(all.toString('utf-8'))
+            try {
+                const ret = Buffer.concat(chunks);
+                resolve(ret.toString('utf-8'))
+
+            } catch (error) {
+                console.log('reduce error:');
+                console.log('reduce error:', chunks[0], chunks.length, error);
+            }
         });
     })
 }
+
+//todo 响应图片，css
