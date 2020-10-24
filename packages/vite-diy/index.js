5a80bd4f62272a9616de985a5e9d0d0485aa1de7
lightnoway <lightnoway@qq.com>
2020-10-24 22:28:04 +0800

    使用koa ，测试中间件 执行

--- packages/vite-diy/index.js
+++ packages/vite-diy/index.js
@@ -1,2 +1,32 @@
 #! /usr/bin/env node
-console.log('hi');
+const Koa = require('koa');
+const send = require('koa-send');
+
+const app = new Koa();
+
+app.use(testMid('1'));
+app.use(testMid('2'));
+app.use(testMid('3'));
+/**
+ *  输出
+1:====
+2:====
+3:====
+3.====
+2.====
+1.====
+ */
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('listen', port);
+})
+
+function testMid(tag) {
+    return function (ctx,next) {
+        console.log(`${tag}:====`)
+        next()
+        console.log(`${tag}.====`)
+    }
+}
+//
